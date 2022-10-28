f3f2c4268f4c87cd16daa3a696816435a23aee0d
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2022-10-27 22:30:02 -0300

    feat: project skeleton

---
+++ src/main/java/ar/edu/ungs/stylebus/StylebusApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ungs.stylebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylebusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylebusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/ungs/stylebus/StylebusApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ungs.stylebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
